b61d298ef8ca592b04e3b4695d7470e198f5b188
japippppp <71950240+favianzhafif26@users.noreply.github.com>
2025-05-14 12:19:24 +0700

    Updating the changes at main.rs that didn't get pushed when doing commit "Simulation slow subscriber"

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
         let ten_millis = time::Duration::from_millis(1000);
         let now = time::Instant::now();
 
-        // thread::sleep(ten_millis);
+        thread::sleep(ten_millis);
 
         println!("In Favian’s Computer 2306274996. Message received: {:?}",
                  message);
